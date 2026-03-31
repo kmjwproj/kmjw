5f4f14cfc3ad61601e6d502942dd3e8b538a0628
hanseungheon <110355039+hhhaan@users.noreply.github.com>
2026-03-31 13:42:23 +0900

    refactor: 스타일 파일 경로 정리

--- src/widgets/bottom-tab-bar/index.tsx
+++ src/widgets/bottom-tab-bar/index.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { cn } from '@/lib/utils'
+import { cn } from "@/src/shared/lib/cn";
 import { TABS } from '@/src/shared/navigation/tab-config'
 
 export default function BottomTabBar() {
